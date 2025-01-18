72299112bf9da8bdcfb433bc4fdc9904f69a05ca
ramihameed2000 <ramihameed2000@gmail.com>
2025-01-18 03:33:31 +0300

    latest

---
+++ ResturantApp/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResturantApp.Data;
+using ResturantApp.Models;
+
+namespace ResturantApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private Repository<Product> Product;
+
+        private Repository<Ingredient> Ingredients;
+
+        private Repository<Category> Category;
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.Product = new Repository<Product>(context);
+            this.Ingredients = new Repository<Ingredient>(context);
+            this.Category = new Repository<Category>(context);
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await Product.GetAllAsync());
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await Ingredients.GetAllAsync();
+            ViewBag.Category = await Category.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                ViewBag.Operation = "Edit";
+                return View();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+    
+
