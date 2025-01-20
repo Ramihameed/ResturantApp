5044ef6e9bc7a81e59920ac3f1d00adc1c3d32c3
ramihameed2000 <ramihameed2000@gmail.com>
2025-01-20 22:19:35 +0300

    the last for now

--- ResturantApp/Controllers/HomeController.cs
+++ ResturantApp/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ResturantApp.Models;
-
+// home
 namespace ResturantApp.Controllers
 {
     public class HomeController : Controller
